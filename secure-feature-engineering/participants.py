12b8074c86c7b75b3fbb577909c21e7328e63eca
shadowyee <1295936221@qq.com>
2024-02-20 13:30:24 +0800

    feat: Add comment on init_parties & parties_info method

--- secure-feature-engineering/participants.py
+++ secure-feature-engineering/participants.py
@@ -9,11 +9,17 @@ class Parties():
         self.crypto_provider = None
 
     def init_parties(self, owner_names, crypto_name):
+        """
+        Initialize the data onwers and crypto provider
+        """
         for people in owner_names:
             self.data_owners.append(sy.VirtualWorker(hook, id=people))
 
         self.crypto_provider = sy.VirtualWorker(hook, id=crypto_name) 
 
     def parties_info(self):
+        """
+        Print all parties information
+        """
         print(self.data_owners)
         print(self.crypto_provider)
