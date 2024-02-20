987a19ff313b45b563a1a93d1873d8141fae9cc3
shadowyee <1295936221@qq.com>
2024-02-20 13:10:44 +0800

    feat: Add dataloader class for MNIST dataset

---
+++ secure-feature-engineering/dataloader.py
@@ -0,0 +1,39 @@
+import torch
+from torchvision import datasets, transforms
+import syft as sy 
+
+class Dataloader():
+    def __init__(self):
+        self.dataset = None
+        self.loader = None
+
+    def load_dataset(self, dataset, isNormalize=False, mean=0.1307, std=0.3081):
+        """
+        Load dataset with normalizing
+        """
+        if dataset == "MNIST":
+            if isNormalize:
+                transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((mean,), (std,))
+                ])
+                print("Normalizing...")
+            else:
+                transform = transforms.Compose([
+                    transforms.ToTensor()
+                ])
+                print("Not normalizing...")
+
+            return self._mnist_loader(transform=transform)
+        else:
+            raise Exception("The dataset not found, MNIST is supported only")
+            
+        
+    def _mnist_loader(self, transform):
+        """
+        Load MINST dataset
+        """
+        # TODO: support more args change
+        self.dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+        self.loader = torch.utils.data.DataLoader(self.dataset, batch_size=len(self.dataset), shuffle=False)
+        return self.loader
