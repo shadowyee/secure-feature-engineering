bd84ebd052d2d1a434ab1eb3f57ffbae3e22f974
shadowyee <1295936221@qq.com>
2024-02-20 12:20:01 +0800

    feat: Add Parties class for secure computation

--- secure-feature-engineering/participants.py
+++ secure-feature-engineering/participants.py
@@ -1,3 +1,19 @@
 import torch
 import syft as sy
 
+hook = sy.TorchHook(torch)
+
+class Parties():
+    def __init__(self):
+        self.data_owners = []
+        self.crypto_provider = None
+
+    def init_parties(self, owner_names, crypto_name):
+        for people in owner_names:
+            self.data_owners.append(sy.VirtualWorker(hook, id=people))
+
+        self.crypto_provider = sy.VirtualWorker(hook, id=crypto_name) 
+
+    def parties_info(self):
+        print(self.data_owners)
+        print(self.crypto_provider)
