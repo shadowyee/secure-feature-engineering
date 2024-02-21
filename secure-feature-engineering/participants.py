9ef422cee8c453dd8497f4c3c27bbc8579ffeb43
shadowyee <1295936221@qq.com>
2024-02-21 12:37:03 +0800

    fix: Make hook variable as a attr of Parties class

--- secure-feature-engineering/participants.py
+++ secure-feature-engineering/participants.py
@@ -1,21 +1,20 @@
 import torch
 import syft as sy
 
-hook = sy.TorchHook(torch)
-
 class Parties():
     def __init__(self):
         self.data_owners = []
         self.crypto_provider = None
+        self.hook = sy.TorchHook(torch)
 
     def init_parties(self, owner_names, crypto_name):
         """
         Initialize the data onwers and crypto provider
         """
         for people in owner_names:
-            self.data_owners.append(sy.VirtualWorker(hook, id=people))
+            self.data_owners.append(sy.VirtualWorker(self.hook, id=people))
 
-        self.crypto_provider = sy.VirtualWorker(hook, id=crypto_name) 
+        self.crypto_provider = sy.VirtualWorker(self.hook, id=crypto_name) 
 
     def parties_info(self):
         """
