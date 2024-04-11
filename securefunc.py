9f62f480c0b76d43420b7850d6a284949f6c4814
shadowyee <1295936221@qq.com>
2024-04-11 15:30:22 +0800

    feat: Add typo judgement logic & getattr to call the func

--- securefunc.py
+++ securefunc.py
@@ -1,20 +1,35 @@
 import torch
 import syft as sy
+import sys
 
-def type_judge():
+def isAdditiveShare(x):
     #TODO: try to ensure the variable has the type of shares
-    pass
+    if isinstance(x, torch.Tensor) and torch.is_tensor(x.child) and isinstance(x.child, sy.AdditiveSharingTensor):
+        return True
+    return False
 
-def secure_add(x: torch.Tensor, y: torch.Tensor):
+def secure_compute(x, y, method):
+    """
+    Secure computation based on fss
+    Only support two parties
+
+    TODO: support more parties, the attrs have to be change like (tuple, method_name) to support more parties
+    """
+    if isAdditiveShare(x) and isAdditiveShare(y):
+        func = getattr(sys.modules[__name__], method)
+        return func(x, y)
+    else:
+        raise TypeError("Only addtive share is allowed")
+
+def secure_add(x, y):
     """
     Add function based on secret sharing
     
     Here use the in-built ADD method in pysyft
     """
-
     return x + y
 
-def secure_eq(x: torch.Tensor, y: torch.Tensor):
+def secure_eq(x, y):
     """
     Determine whether is two value are equal based on secret sharing
     
@@ -23,7 +38,7 @@ def secure_eq(x: torch.Tensor, y: torch.Tensor):
 
     return x == y
 
-def secure_lt(x: torch.Tensor, y: torch.Tensor):
+def secure_lt(x, y):
     """
     Determine whether is fisrt value is less than second value based on secret sharing
 
@@ -32,7 +47,7 @@ def secure_lt(x: torch.Tensor, y: torch.Tensor):
 
     return x < y
 
-def secure_mul(x: torch.Tensor, y: torch.Tensor):
+def secure_mul(x, y):
     """
     Multipication function based on secret sharing
 
@@ -41,7 +56,7 @@ def secure_mul(x: torch.Tensor, y: torch.Tensor):
     
     return x * y
 
-def secure_sqrt(x: torch.Tensor, y: torch.Tensor):
+def secure_sqrt(x):
     """
     Square root operation based on secret sharing
 
@@ -49,7 +64,7 @@ def secure_sqrt(x: torch.Tensor, y: torch.Tensor):
     """
     pass
 
-def secure_div(x: torch.Tensor, y: torch.Tensor):
+def secure_div(x, y):
     """
     Division function based on secret sharing
 
@@ -57,7 +72,7 @@ def secure_div(x: torch.Tensor, y: torch.Tensor):
 
     Here we don't use the origin in-built div method in pysyft
     """
-    pass
+    return x / y
 
 #TODO: implement more methods althought they are not use in the current project
 
