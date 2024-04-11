c5e39b6750a5b9cac711a54f0515d53618e69c90
shadowyee <1295936221@qq.com>
2024-04-11 16:17:17 +0800

    feat: Add correction test for secure function

---
+++ test/securefunc-test.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append('..')
+
+import securefunc as sfc
+import syft as sy
+import torch
+
+hook = sy.TorchHook(torch)
+
+alice = sy.VirtualWorker(hook, id="alice")
+bob = sy.VirtualWorker(hook, id="bob")
+
+def correction_test():
+    """
+    Test the correction of every function
+    """
+    # x = torch.tensor([25]).share(bob,alice)
+    # y = torch.tensor([5]).share(bob,alice)
+
+    crypto_provider = sy.VirtualWorker(hook, id="crypto_provider")   # 利用可信第三方生成乘法三元组
+    x = torch.tensor([25]).share(bob,alice, crypto_provider=crypto_provider)
+    y = torch.tensor([5]).share(bob,alice, crypto_provider=crypto_provider)
+
+    # Add function
+    add = sfc.secure_compute(x, y, "add")
+    print("secure addition:", add.get())
+
+    # Equal function
+    eq = sfc.secure_compute(x, y, "eq")
+    print("secure equation:", eq.get())
+
+    # Less Than function
+    lt = sfc.secure_compute(x, y, "lt")
+    print("secure less than:", lt.get())
+
+    # Multiplication function
+    mul = sfc.secure_compute(x, y, "mul")
+    print("secure mul:", mul.get())
+
+    # TODO: Square Root function
+
+    # Division function
+    div = sfc.secure_compute(x, y, "div")
+    print("secure div:", div.get())
+
+if __name__ == "__main__":
+    print("========Correction Test========")
+    correction_test()
