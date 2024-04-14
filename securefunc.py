196c77cad254d63faefce845e60104ee543dd669
shadowyee <1295936221@qq.com>
2024-04-14 11:54:17 +0800

    feat: Implement division by mul & truncate

--- securefunc.py
+++ securefunc.py
@@ -9,7 +9,7 @@ def isAdditiveShare(x):
         return True
     return False
 
-def secure_compute(x, y, method):
+def secure_compute(x, y, method, prec):
     """
     Secure computation based on fss
     Only support two parties
@@ -17,10 +17,13 @@ def secure_compute(x, y, method):
     TODO: support more parties, the attrs have to be change like (tuple, method_name) to support more parties
     """
 
-    if isAdditiveShare(x) and isAdditiveShare(y):
+    if isAdditiveShare(x) and (isAdditiveShare(y) or isinstance(y, int)):
         name = "__secure_" + method
         func = getattr(sys.modules[__name__], name)
-        return func(x, y)
+        if method == "div":
+            return func(x, y, prec)
+        else:
+            return func(x, y)
     else:
         raise TypeError("Only additive share is allowed")
 
@@ -59,6 +62,21 @@ def __secure_mul(x, y):
     
     return x * y
 
+def __secure_exp(x, y):
+    """
+    Exponentation function based on scret sharing
+
+    Example:
+        x, y has to be integer
+        return x^y
+
+    TODO: use fast exponentation to implement it
+    """
+
+    
+    pass
+
+
 def __secure_sqrt(x):
     """
     Square root operation based on secret sharing
@@ -67,7 +85,7 @@ def __secure_sqrt(x):
     """
     pass
 
-def __secure_div(x, y):
+def __secure_div(x, y, prec):
     """
     Division function based on secret sharing
 
@@ -76,7 +94,54 @@ def __secure_div(x, y):
     Here we don't use the origin in-built div method in pysyft
     The in-built div method cost a lot
     """
-    return x / y
+    if isAdditiveShare(y):
+        return x / y
+    else:
+        return __division(x, y, prec)
+
+def __division(x, y, prec):
+    """
+    Implement division by multiplation and truncate
+    """
+    if not isAdditiveShare(x):
+        raise AttributeError("The dividend is not Additive Share")
+    if not isinstance(y, int):
+        raise AttributeError("The divisor is not integer")
+
+    ret = x * (1 / y * pow(10, prec))
+
+    return ret
+
+
+
+
+
+
+def __division_newton(a, b, precision=1e-3, max_iter=1000):
+    """
+    使用牛顿迭代法实现除法
+    """
+    # if b == 0:
+    #     raise ValueError("除数不能为零")
+
+    # 定义牛顿迭代函数
+    def f(x):
+        return a - (1 / b) * x
+
+    # 定义牛顿迭代的导数函数
+    def df(x):
+        return -1 / b
+
+    # 初始化迭代起点
+    x = 1.0
+
+    # 进行牛顿迭代
+    for _ in range(max_iter):
+        x_next = x - f(x) / df(x)
+
+        x = x_next
+
+    return x_next
 
 #TODO: implement more methods althought they are not use in the current project
 
