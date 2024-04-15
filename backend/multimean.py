c5365b243c7b3d4ea33b260d00724b9603bc34a4
shadowyee <1295936221@qq.com>
2024-04-15 16:09:55 +0800

    feat: Implement the secure mean computation with file as input

---
+++ backend/multimean.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('..')
+
+import securestatis as sst
+import participants as pt
+import syft as sy
+import torch
+
+def file_to_tensor(file_path):
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    matrix = []
+    for line in lines:
+        row = [float(x) for x in line.split()]  
+        matrix.append(row)
+
+    tensor = torch.tensor(matrix, dtype=torch.float32)
+
+    return tensor
+
+def secure_mean_compute():
+    # data = torch.tensor([[1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6, 7], [2, 3, 4, 5, 6, 7]])
+    file_path = 'uploads/test.txt'  
+    data = file_to_tensor(file_path)
+
+    data_owners = []
+    parties_num = 8
+    for i in range(0, parties_num):
+        data_owners.append("workers" + str(i))
+    crypto_provider = "crypro_provider"
+
+    parties = pt.Parties()
+    parties.init_parties(data_owners, crypto_provider)
+
+    fix_prec = 3
+    prec = 5
+    mean = sst.secure_mean(parties.data_owners, parties.crypto_provider, data, prec)
+
+    for m in mean:
+        print("The mean of data:", float(m.get())/pow(10, fix_prec + prec))
+
+
+if __name__ == "__main__":
+    secure_mean_compute()
+
