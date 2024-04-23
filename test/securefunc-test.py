dd70296b8ece5caa32447f70e017df40361c745a
shadowyee <1295936221@qq.com>
2024-04-23 22:09:12 +0800

    feat: Add secure max method test

--- test/securefunc-test.py
+++ test/securefunc-test.py
@@ -10,6 +10,18 @@ hook = sy.TorchHook(torch)
 alice = sy.VirtualWorker(hook, id="alice")
 bob = sy.VirtualWorker(hook, id="bob")
 
+def get_worker_share(worker):
+    total_size = 0
+    objects = []
+    for obj_id in worker._objects:
+        obj = worker._objects[obj_id]
+        if obj.shape == torch.Size([]):
+            objects.append(int(obj))
+        else:
+            objects.append(tuple(obj.tolist()))
+        total_size += obj.__sizeof__()
+    return objects, total_size
+
 def correction_test():
     """
     Test the correction of every function
@@ -58,9 +70,33 @@ def mean_test():
     z = s * 1000 / y
     print(z.get())
 
+def max_test():
+    crypto_provider = sy.VirtualWorker(hook, id="crypto_provider")   # 利用可信第三方生成乘法三元组
+    x = torch.tensor(1).share(bob,alice, crypto_provider=crypto_provider)
+    y = torch.tensor(2).share(bob,alice, crypto_provider=crypto_provider)
+    z = torch.tensor(3).share(bob,alice, crypto_provider=crypto_provider)
+    shares = [x, y, z]
+    
+    objects, objects_total_size = get_worker_share(alice)
+    print(objects)
+    
+    ret = sfc.secure_max(shares)
+    print(shares)
+    shares = sfc.remove_share(shares, ret)
+    print(shares)
+    print(ret.get())
+
+    # exec secure_max() thirdly
+    ret = sfc.secure_max(shares)
+    print(ret.get())
+
+    objects, objects_total_size = get_worker_share(alice)
+    print(objects)
+
 if __name__ == "__main__":
     # print("========Correction Test========")
     # correction_test()
 
-    reciprocal_test()
+    # reciprocal_test()
     # mean_test()
+    max_test()
