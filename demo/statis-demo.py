b1f6ea350e5d7c95ade036e4ef416604af53a7e7
shadowyee <1295936221@qq.com>
2024-05-08 22:46:22 +0800

    feat: Finish median computation

--- demo/statis-demo.py
+++ demo/statis-demo.py
@@ -23,14 +23,81 @@ def reciprocal_sqrt_newton(x):
     y = math.exp(-2.2*(x/2 + 0.2)) + 0.198046875
     y_sh = torch.tensor(y).fix_precision().share(bob,alice,crypto_provider=crypto_provider)
     x_sh = torch.tensor(x/2).fix_precision().share(bob,alice,crypto_provider=crypto_provider)
+    
     for i in range(3):
         y_sh = y_sh * (1.5 - x_sh * y_sh * y_sh)
         # y_sh = sfc.__division(y_sh, 2, prec)
-    print(y_sh.get())
+    z = y_sh * x_sh
+    print(z.get()*2)
 
 def sqrt_newton():
     alice = sy.VirtualWorker(hook, id="alice")
     bob = sy.VirtualWorker(hook, id="bob")
 
+def secure_median():
+    alice = sy.VirtualWorker(hook, id="alice")
+    bob = sy.VirtualWorker(hook, id="bob")
+    crypto_provider = sy.VirtualWorker(hook, id="crypto_provider")
+    numList = torch.Tensor([1, 2, 3, 4])
+    shares = numList.fix_precision().share(alice, bob, crypto_provider=crypto_provider)
+    # numList = [1, 2, 3, 4, 5]
+    # shares = []
+    # for num in numList:
+    #     shares.append(torch.Tensor(num).fix_precision().share(alice, bob, crypto_provider=crypto_provider))
+
+    print("======== Bits Vector Generation ========")
+    sort_vector = []
+    N = len(numList)
+
+    for i in range(N):
+        sum = shares[i] > shares[i]
+        for j in range(N):
+            if j == i:
+                continue
+            sum += shares[i] > shares[j]
+        sort_vector.append(sum)
+
+    bit_vector = []
+    mid = torch.Tensor([N // 2]).fix_precision().share(alice, bob, crypto_provider=crypto_provider)
+    print(mid.copy().get())
+    
+    if N % 2 == 1:
+        for i in range(N):
+            print(sort_vector[i].copy().get())
+            z = sort_vector[i] == mid
+            print(z.copy().get())
+            bit_vector.append(z)
+        
+        print("======== Print Bits Vector ========")
+        for i in range(N):
+            print(bit_vector[i].copy().get())
+
+        median = torch.tensor([0]).fix_precision().share(alice, bob, crypto_provider=crypto_provider)
+        for i in range(N):
+            median += shares[i] * bit_vector[i]
+        
+        print("======== Median ========")
+        print(median.get())
+    else:
+        for i in range(N):
+            mid_p = torch.Tensor([N // 2 - 1]).fix_precision().share(alice, bob, crypto_provider=crypto_provider)
+            print(sort_vector[i].copy().get())
+            z = (sort_vector[i] == mid) + (sort_vector[i] == mid_p)
+            print(z.copy().get())
+            bit_vector.append(z)
+        
+        print("======== Print Bits Vector ========")
+        for i in range(N):
+            print(bit_vector[i].copy().get())
+
+        median = torch.tensor([0]).fix_precision().share(alice, bob, crypto_provider=crypto_provider)
+        for i in range(N):
+            median += shares[i] * bit_vector[i]
+        
+        median = median / 2
+
+        print("======== Median ========")
+        print(median.get())
 if __name__ == "__main__":
-    reciprocal_sqrt_newton(36)
+    # reciprocal_sqrt_newton(36)
+    secure_median()
